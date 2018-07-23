767147821c779b3702972188b54f5760b2a52626
zhangbufeng <1372078654@qq.com>
2018-07-23 23:57:12 +0800

    zbf

--- js/index.js
+++ js/index.js
@@ -1,8 +1,8 @@
 /*
 * @Author: dapang
 * @Date:   2018-07-22 12:38:43
-* @Last Modified by:   dapang
-* @Last Modified time: 2018-07-22 14:07:40
+* @Last Modified by:   zhangbufeng
+* @Last Modified time: 2018-07-23 23:55:07
 */
 window.onload=function(){
 	let aside=document.getElementsByClassName("aside")[0];
@@ -21,6 +21,30 @@ window.onload=function(){
 		}
 	}
 
-
+	let bigBox=document.getElementsByClassName("bigBox")[0];
+    let background=bigBox.getElementsByClassName("background")[0];
+    let banner=background.getElementsByClassName("banner")[0];
+    let warper=banner.getElementsByClassName("warper")[0];
+    let as=warper.getElementsByTagName("a");
+    console.log(as);
+    let num=0;
+    let t=setInterval(move,1000);
+    function move(){
+    	num++;
+    	if(num==10){
+    		for(let i=0;i<as.length;i++){
+    			as[i].style.zIndex=5;
+    			num=0;
+    		}
+    	}
+    	as[num].style.zIndex=10;
+    }
+    banner.onmouseenter=function(){
+    	clearInterval(t);
+    }
+    banner.onmouseleave=function(){
+    	t=setInterval(move,2000);
+    }
+    
 
 }
